a14b628a92da65dc6622d63b1aa0a5cb90428171
xyder0 <zacxyder@gmail.com>
2025-07-15 23:51:50 +0530

    chore: Initialized PortAudio

--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,13 @@
 #include <portaudio.h>
 
 int main(){
-	std::cout << "kitty-play: Hello world";
+	PaError err = Pa_Initialize();
+	if(err != paNoError){
+		std::cerr << "PortAudio init failed: " << Pa_GetErrorText(err) << "\n";
+		return 1;
+	}
+	std::cout << "Portaudio intialized successfully";
+	Pa_Terminate();
+	return 0;
 }
+
